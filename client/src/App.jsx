463c008585386e764e76db46cc331e32d86787c1
sgadzhanov <101121271+sgadzhanov@users.noreply.github.com>
2024-07-08 16:36:40 +0300

    refactor App.tsx code; extract logic into custom hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,123 +1,20 @@
 import React, { useEffect, useState } from "react"
-import PhotoItem from "../components/PhotoItem"
+import PhotoItem from "./components/PhotoItem"
 import "./App.css"
+import { usePhotos } from "./hooks/usePhotos"
 
 function App() {
-  const [photos, setPhotos] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-    image: null,
-  })
-  const [error, setError] = useState(false)
-
-  async function fetchPhotos() {
-    setIsLoading(true)
-    try {
-      const response = await fetch("http://localhost:5000/api/photos")
-      const data = await response.json()
-      setPhotos(data)
-    } catch (error) {
-      console.error("Error fetching photos:", error)
-      setError(true)
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    fetchPhotos()
-  }, [])
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }))
-  }
-
-  const handleFileChange = (e) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      image: e.target.files[0],
-    }))
-  }
-
-  const submitHandler = async (e) => {
-    e.preventDefault()
-    setIsLoading(true)
-
-    const formDataToSubmit = new FormData()
-    formDataToSubmit.append("title", formData.title)
-    formDataToSubmit.append("description", formData.description)
-    formDataToSubmit.append("image", formData.image)
-
-    try {
-      const response = await fetch("http://localhost:5000/api/photos", {
-        method: "POST",
-        body: formDataToSubmit,
-      })
-
-      if (!response.ok) {
-        throw new Error("Failed to upload photo")
-      }
-
-      setIsLoading(false)
-      setFormData({ title: "", description: "", image: null })
-      fetchPhotos()
-    } catch (error) {
-      console.error("Error uploading photo:", error)
-      setError(true)
-      setIsLoading(false)
-    }
-  }
-
-  const deleteHandler = async (id) => {
-    setIsLoading(true)
-    try {
-      const response = await fetch(`http://localhost:5000/api/photos/${id}`, {
-        method: "DELETE",
-      })
-      if (!response.ok) {
-        throw new Error("Failed to delete photo")
-      }
-      setPhotos((prevPhotos) => prevPhotos.filter((photo) => photo._id !== id))
-    } catch (error) {
-      console.error("Error deleting photo:", error)
-      setError(true)
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
-  const handleEdit = async (editedPhoto) => {
-    setIsLoading(true)
-    try {
-      const response = await fetch(
-        `http://localhost:5000/api/photos/${editedPhoto._id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(editedPhoto),
-        }
-      )
-
-      if (!response.ok) {
-        throw new Error("Failed to update photo")
-      }
-
-      setIsLoading(false)
-      fetchPhotos()
-    } catch (error) {
-      console.error("Error updating photo:", error)
-      setError(true)
-      setIsLoading(false)
-    }
-  }
+const {
+  photos,
+  isLoading,
+  handleInputChange,
+  formData,
+  handleFileChange,
+  deleteHandler,
+  handleEdit,
+  submitHandler,
+  error,
+} = usePhotos()
 
   if (isLoading) {
     return <p style={{ textAlign: "center" }}>Loading...</p>
